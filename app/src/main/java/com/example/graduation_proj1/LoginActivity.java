f79b8a3fd0facc819f999e083fdb8605b0c1525d
soyoung <2011635@sookmyung.ac.kr>
2023-08-05 00:26:15 +0900

    login done and itemlist create

--- app/src/main/java/com/example/graduation_proj1/LoginActivity.java
+++ app/src/main/java/com/example/graduation_proj1/LoginActivity.java
@@ -7,9 +7,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
@@ -24,8 +22,29 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), FindPasswordActivity.class);
                 startActivity(intent);
             }
+
+
         });
 
+        // 회원가입 버튼 클릭 시 액티비티 전환
+        Button register_button = (Button) findViewById(R.id.button6);
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //로그인 버튼 클릭 시 액티비티 전환
+        Button login_button = (Button) findViewById(R.id.button4);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v {
+                Intent intent = new Intent(getApplicationContext(), ItemlistActivity.class);
+                startActivity(intent);
+            })
+        });
 
     }
 }
