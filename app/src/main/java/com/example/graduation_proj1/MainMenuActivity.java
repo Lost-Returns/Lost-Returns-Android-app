f6caba982e88c3ebea674244e971637e76213cd4
Jaeeun <pjcson8@naver.com>
2023-08-06 02:02:25 +0900

    feat: bottom navigation bar - before login

---
+++ app/src/main/java/com/example/graduation_proj1/MainMenuActivity.java
@@ -0,0 +1,67 @@
+package com.example.graduation_proj1;
+
+import android.content.ClipData;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.graduation_proj1.databinding.ActivityMainBinding;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    //ActivityMainBinding binding;
+    private BottomNavigationView bottomNavigationView;
+
+    private static final int MENU_HOME = R.id.fragment_main_menu_home;
+    private static final int MENU_REGIST = R.id.fragment_main_menu_regist;
+    private static final int MENU_MY = R.id.fragment_main_menu_my;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        //(binding.getRoot());
+        if(isUserLoggedIn()) {
+
+            replaceFragment(new MainMenuHomeFragment()); // 기본으로 HomeFragment 화면 표시
+
+            bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+            bottomNavigationView.setOnItemSelectedListener(item -> {
+                int id = item.getItemId();
+                if (id == MENU_HOME) {
+                    replaceFragment(new MainMenuHomeFragment());
+                } else if (id == MENU_REGIST) {
+                    replaceFragment(new MainMenuRegistFragment());
+                } else if (id == MENU_MY) {
+                    replaceFragment(new MainMenuMyFragment());
+                }
+
+                return true;
+
+            });
+        }
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    private boolean isUserLoggedIn(){
+        //로그인 상태를 체크하는 로직 구현
+
+        return false;
+    }
+}
