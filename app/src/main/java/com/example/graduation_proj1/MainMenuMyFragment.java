2317ff89ec6f8fc03fb64ebf981fde3a384a4cad
soyoung <2011635@sookmyung.ac.kr>
2023-08-09 16:25:14 +0900

    userinfo

--- app/src/main/java/com/example/graduation_proj1/MainMenuMyFragment.java
+++ app/src/main/java/com/example/graduation_proj1/MainMenuMyFragment.java
@@ -6,25 +6,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Button;
-import android.content.Intent;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
+import com.example.graduation_proj1.MainMenuActivity;
+import com.example.graduation_proj1.MyProfileFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainMenuMyFragment extends Fragment {
 
     private TextView usernameTextView;
     private DatabaseReference userRef;
+    private TextView userNameTextView;
 
     public static MainMenuMyFragment newInstance(){
         return new MainMenuMyFragment();
@@ -33,31 +34,45 @@ public class MainMenuMyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_main_menu_my, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main_menu_my, container, false);
 
-        usernameTextView = rootView.findViewById(R.id.usernameTextView);
+        usernameTextView = rootView.findViewById(R.id.userNameTextView);
 
         // 사용자 이름 불러와 TextView에 표시
         FirebaseAuth auth = FirebaseAuth.getInstance();
-        String userId = auth.getCurrentUser().getUid();
+        FirebaseUser user = auth.getCurrentUser();
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        userRef = database.getReference("users").child(userId);
+        if (user != null) {
+            String userId = user.getUid();
 
-        userRef.child("name").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    String userName = dataSnapshot.getValue(String.class);
-                    usernameTextView.setText("사용자 이름: " + userName);
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            userRef = database.getReference("users").child(userId);
+            userNameTextView = rootView.findViewById(R.id.userNameTextView);
+
+            // Firestore에서 사용자 이름 가져오기
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            DocumentReference userDocRef = db.collection("users").document(userId); // 변경: uid 대신 userId 사용
+
+            userDocRef.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String userName = document.getString("name"); // 사용자 이름
+
+                        // 사용자 이름 및 전화번호 표시
+                        if (userName != null) {
+                            userNameTextView.setText(userName);
+                        } else {
+                            userNameTextView.setText("이름 없음");
+                        }
+                    }
                 }
-            }
+            });
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // 오류 처리
-            }
-        });
+        } else {
+            // 사용자가 인증되지 않았거나 로그인되지 않은 상태
+            // 이 부분에 대한 처리가 필요하다면 여기에 작성
+        }
 
         // 프로필 수정 버튼 클릭 시 액티비티 전환
         Button edit_profile_button = rootView.findViewById(R.id.editProfile_btn);
@@ -71,5 +86,4 @@ public class MainMenuMyFragment extends Fragment {
 
         return rootView;
     }
-
 }
--- app/src/main/java/com/example/graduation_proj1/MyProfileFragment.java
+++ app/src/main/java/com/example/graduation_proj1/MyProfileFragment.java
@@ -1,33 +1,100 @@
 package com.example.graduation_proj1;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.graduation_proj1.MyActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MyProfileFragment extends Fragment {
+
 
     public static MyProfileFragment newInstance() {
         return new MyProfileFragment();
     }
 
     private Button reviseButton;
+    private TextView userEmailTextView;
+    private TextView userNameTextView;
+    private TextView userContactTextView;
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_my_profile, container, false);
+    @SuppressLint("MissingInflatedId")
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_my_profile, container, false);
 
-        //필요한 코드 넣기 - MyProfileActivity.java에 있던 코드를 Fragment 형식으로 변형해서 가져오기.
+        reviseButton = rootView.findViewById(R.id.button7);
+        reviseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // MainMenuActivity로 이동하고 MainMenuMyFragment를 표시하기 위한 인텐트 생성
+                Intent intent = new Intent(requireContext(), MainMenuActivity.class);
+                intent.putExtra("selectedFragment", "MainMenuMyFragment"); // 이 정보를 통해 프래그먼트를 선택하도록 처리할 수 있어야 합니다.
+                startActivity(intent);
+            }
 
+        });
 
+        userEmailTextView = rootView.findViewById(R.id.userEmailTextView);
+        userNameTextView = rootView.findViewById(R.id.userNameTextView);
+        userContactTextView = rootView.findViewById(R.id.userContactTextView);
+
+        // Firebase 사용자 정보 가져오기
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+
+        if (user != null) {
+            String userEmail = user.getEmail(); // 사용자 이메일
+            String uid = user.getUid(); // 사용자 UID
+
+            // 사용자 이메일 표시
+            userEmailTextView.setText(userEmail);
+
+            // Firestore에서 사용자 이름 및 전화번호 가져오기
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            DocumentReference userDocRef = db.collection("users").document(uid);
+
+            userDocRef.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String userName = document.getString("name"); // 사용자 이름
+                        String userContract = document.getString("contact"); // 사용자 전화번호
+
+                        // 사용자 이름 및 전화번호 표시
+                        if (userName != null) {
+                            userNameTextView.setText(userName);
+                        } else {
+                            userNameTextView.setText("이름 없음");
+                        }
+
+                        if (userContract != null) {
+                            userContactTextView.setText(userContract);
+                        } else {
+                            userContactTextView.setText("전화번호 없음");
+                        }
+                    }
+                }
+            });
+
+        } else {
+            // 사용자가 인증되지 않았거나 로그인되지 않은 상태
+        }
 
         return rootView;
     }
